eb942f5b2ea12e3aa08a1b2af73698c2c85cc491
diegopaniago <diegopaniago1@gmail.com>
2024-08-01 23:11:46 -0400

    feat: Adding css style and handling errors

--- frontend/src/components/currencyConverter/currencyConverter.tsx
+++ frontend/src/components/currencyConverter/currencyConverter.tsx
@@ -1,25 +1,42 @@
-import React, {  useState } from "react";
-import { getCurrency } from "../../apis/currencyApi";
+import React, {  useState } from 'react';
+import { getCurrency } from '../../apis/currencyApi';
+import './currencyConverter.css';
 
 async function convert(baseCoin: string, targetCoin: string, setCurrency: any): Promise<void> {
-  const response = await getCurrency(baseCoin, targetCoin);
-  setCurrency(JSON.stringify(response));
+  try {
+    const response = await getCurrency(baseCoin, targetCoin);
+    setCurrency(JSON.stringify(response));
+  } catch (error) {
+    alert('Error while converting currency!');
+    console.error(error);
+  }
+}
+
+function typeHandler(event: React.ChangeEvent<HTMLInputElement>, callback: any) {
+  event.target.value = event.target.value.toUpperCase();
+  callback(event.target.value);
 }
 
 
 const CurrencyConverter: React.FC = () => {
-  const [baseCoin, setBaseCoin] = useState("");
-  const [targetCoin, setTargetCoin] = useState("");
-  const [currency, setCurrency] = useState("");
+  const [baseCoin, setBaseCoin] = useState('');
+  const [targetCoin, setTargetCoin] = useState('');
+  const [currency, setCurrency] = useState('');
   
   return (
-      <div>
-        <label>Base coin:</label>
-        <input type="text" onChange={(event) => setBaseCoin(event?.target?.value || '')}/>
-        <label>Target coin:</label>
-        <input type="text" onChange={(event) => setTargetCoin(event?.target?.value || '')} />
-        <button onClick={() => convert(baseCoin, targetCoin, setCurrency)}>Convert</button>
-        <textarea value={currency}></textarea>
+      <div className='currency-box'>
+        <div>
+          <label>Base coin:</label>
+          <input type='text' onChange={(event) => typeHandler(event, setBaseCoin)}/>
+        </div>
+        <div>
+          <label>Target coin:</label>
+          <input type='text' onChange={(event) => typeHandler(event, setTargetCoin)} />
+        </div>
+        <div>
+          <button onClick={() => convert(baseCoin, targetCoin, setCurrency)}>Convert</button>
+        </div>
+        <textarea disabled value={currency}></textarea>
       </div>
     );
   }
