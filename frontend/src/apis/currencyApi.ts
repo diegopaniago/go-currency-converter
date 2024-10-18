1b3fc0ad5f1db97926f729a406bce1fc796c1887
diegopaniago <diegopaniago1@gmail.com>
2024-10-17 23:31:00 -0400

    feat: Adding style to the convert currency component

--- frontend/src/apis/currencyApi.ts
+++ frontend/src/apis/currencyApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 async function getCurrency(baseCoin: string, targetCoin: string): Promise<any> {
-    const response = await axios.get(`http://localhost:5001/currency/${baseCoin}/${targetCoin}`)
+    const response = await axios.get(`http://localhost:5001/currency/${baseCoin}?targets=${targetCoin}`)
     return response.data
 }
 
--- frontend/src/components/currencyConverter/currencyConverter.tsx
+++ frontend/src/components/currencyConverter/currencyConverter.tsx
@@ -1,18 +1,18 @@
 import React, {  useState } from 'react';
 import { getCurrency } from '../../apis/currencyApi';
-import './currencyConverter.css';
 
 async function convert(baseCoin: string, targetCoin: string, setCurrency: any): Promise<void> {
   try {
     const response = await getCurrency(baseCoin, targetCoin);
-    setCurrency(JSON.stringify(response));
+    console.log('res:', response)
+    setCurrency(response);
   } catch (error) {
     alert('Error while converting currency!');
     console.error(error);
   }
 }
 
-function typeHandler(event: React.ChangeEvent<HTMLInputElement>, callback: any) {
+function typingHandler(event: React.ChangeEvent<HTMLInputElement>, callback: any) {
   event.target.value = event.target.value.toUpperCase();
   callback(event.target.value);
 }
@@ -21,22 +21,51 @@ function typeHandler(event: React.ChangeEvent<HTMLInputElement>, callback: any) 
 const CurrencyConverter: React.FC = () => {
   const [baseCoin, setBaseCoin] = useState('');
   const [targetCoin, setTargetCoin] = useState('');
-  const [currency, setCurrency] = useState('');
+  const [currency, setCurrency] = useState([]);
   
   return (
-      <div className='currency-box'>
-        <div>
-          <label>Base coin:</label>
-          <input type='text' onChange={(event) => typeHandler(event, setBaseCoin)}/>
+      <div className='grid grid-cols-2 p-5'>
+        <div className='col-auto'>
+          <div className='p-1'>
+            <label className='block mb-2 text-sm text-slate-600'>Base coin:</label>
+            <input className='w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow' 
+              type='text' 
+              onChange={(event) => typingHandler(event, setBaseCoin)}/>
+          </div>
+          <div className='p-1' title='Use , to pass many target coins'>
+            <label className='block mb-2 text-sm text-slate-600'>Target coin(s):</label>
+            <input className='w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow' 
+              type='text' 
+              onChange={(event) => typingHandler(event, setTargetCoin)} />
+          </div>
+          <div className='p-1 text-center'>
+            <button 
+              className={`
+                font-bold py-1 px-4 rounded 
+                ${!baseCoin.trim() || !targetCoin.trim() 
+                  ? 'bg-gray-400 cursor-not-allowed'   // Disabled state styles
+                  : 'bg-blue-500 hover:bg-blue-700 text-white'  // Enabled state styles
+                }
+              `} 
+              disabled={!baseCoin.trim() || !targetCoin.trim()}
+              onClick={() => convert(baseCoin, targetCoin, setCurrency)}>Convert</button>
+          </div>
         </div>
-        <div>
-          <label>Target coin:</label>
-          <input type='text' onChange={(event) => typeHandler(event, setTargetCoin)} />
+        <div className='col-auto'>
+          <div className={`
+            ${!baseCoin.trim() || !targetCoin.trim() 
+              ? 'invisible'
+              : ''
+            }
+            bg-blue-500 p-1 rounded`}>
+            <label className='text-white'>Converting 1 {baseCoin} to:</label>
+          </div>
+          {currency.map((c:any) => (
+          <div className='bg-gray-200 m-1 rounded'>
+            <label>{c.exchange.name}: {c.exchange.price}</label>
+          </div>
+          ))}
         </div>
-        <div>
-          <button onClick={() => convert(baseCoin, targetCoin, setCurrency)}>Convert</button>
-        </div>
-        <textarea disabled value={currency}></textarea>
       </div>
     );
   }
